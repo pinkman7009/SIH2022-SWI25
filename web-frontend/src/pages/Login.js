9f097393a05b186d63b454b04c8fb6ed20081f27
pinkman7009 <soumik.chaudhuri2000@gmail.com>
2022-08-25 14:48:00 +0530

    fix: logout bug

--- web-frontend/src/pages/Login.js
+++ web-frontend/src/pages/Login.js
@@ -24,33 +24,35 @@ const Login = () => {
   return (
     <>
       <TopNavbar showLogo={true} isLoggedIn={false} />
-      <div className="bg-secondaryBg min-h-screen w-full flex justify-center items-center">
-        <div className="rounded-3xl shadow-md p-6 w-1/4 flex flex-col items-center justify-evenly bg-white h-[500px]">
-          <h3 className="text-[2rem]">CLTS Login</h3>
-          <div className="mx-auto w-2/3">
-            <input
-              type="email"
-              className="rounded-lg p-3 h-12 w-full border-2 border-primary my-6"
-              placeholder="Enter email"
-              value={email}
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-            />
-            <input
-              type="password"
-              className="rounded-lg p-3 h-12 w-full border-2 border-primary mb-6"
-              placeholder="Enter password"
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            />
+      <form action="" onSubmit={onSubmit}>
+        <div className="bg-secondaryBg min-h-screen w-full flex justify-center items-center">
+          <div className="rounded-3xl shadow-md p-6 w-1/4 flex flex-col items-center justify-evenly bg-white h-[500px]">
+            <h3 className="text-[2rem]">CLTS Login</h3>
+            <div className="mx-auto w-2/3">
+              <input
+                type="email"
+                className="rounded-lg p-3 h-12 w-full border-2 border-primary my-6"
+                placeholder="Enter email"
+                value={email}
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
+              />
+              <input
+                type="password"
+                className="rounded-lg p-3 h-12 w-full border-2 border-primary mb-6"
+                placeholder="Enter password"
+                value={password}
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+            </div>
+            <p className="text-primary">Forgot Password?</p>
+            <PrimaryButton text="Login" size="medium" handleClick={onSubmit} />
           </div>
-          <p className="text-primary">Forgot Password?</p>
-          <PrimaryButton text="Login" size="medium" handleClick={onSubmit} />
         </div>
-      </div>
+      </form>
     </>
   );
 };
