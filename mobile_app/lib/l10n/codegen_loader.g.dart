458f783a162ca21f11c06f0662826eb3f6fe5c7f
NISHNATA DEBNATH <nishnatadebnath@gmail.com>
2022-08-25 17:32:49 +0530

    locale added

--- mobile_app/lib/l10n/codegen_loader.g.dart
+++ mobile_app/lib/l10n/codegen_loader.g.dart
@@ -6,35 +6,154 @@ import 'dart:ui';
 
 import 'package:easy_localization/easy_localization.dart' show AssetLoader;
 
-class CodegenLoader extends AssetLoader{
+class CodegenLoader extends AssetLoader {
   const CodegenLoader();
 
   @override
-  Future<Map<String, dynamic>> load(String fullPath, Locale locale ) {
+  Future<Map<String, dynamic>> load(String fullPath, Locale locale) {
     return Future.value(mapLocales[locale.toString()]);
   }
 
-  static const Map<String,dynamic> en = {
-  "dashboard": "Dashboard",
-  "announcement": "Announcement",
-  "complaint_history": "Complaint History",
-  "help": "Help",
-  "about": "About",
-  "logout": "Logout",
-  "post_complaint": "Post Complaint",
-  "track_status": "Track Status",
-  "child": "Chil"
-};
-static const Map<String,dynamic> hi = {
-  "dashboard": "डैशबोर्ड",
-  "announcement": "घोषणा",
-  "complaint_history": "शिकायत इतिहास",
-  "help": "मदद",
-  "about": "ऐप के बारे में",
-  "logout": "लॉग आउट",
-  "post_complaint": "शिकायत पोस्ट करें",
-  "track_status": "ट्रैक स्थिति",
-  "child": "बच्चा"
-};
-static const Map<String, Map<String,dynamic>> mapLocales = {"en": en, "hi": hi};
+  static const Map<String, dynamic> ta = {
+    "dashboard": "டாஷ்போர்டு",
+    "app_name": "குழந்தை தொழிலாளர் கண்காணிப்பு அமைப்பு",
+    "announcement": "அறிவிப்பு",
+    "help": "உதவி",
+    "about": "தகவல்",
+    "logout": "வெளியேறு",
+    "stats1": "10 பேர் தங்கள் குறைகளைத் தீர்க்க CLTS ஐப் பயன்படுத்துகின்றனர்",
+    "post": "புகாரை இடுங்கள்",
+    "post_line":
+        "குழந்தை தொழிலாளர்கள் குறித்த உங்கள் புகார்களை இங்கே பதிவு செய்யவும்",
+    "track": "நிலையைக் கண்காணிக்கவும்",
+    "track_line":
+        "உங்கள் பதிவு செய்யப்பட்ட புகார்களின் நிலையைச் சரிபார்க்கவும்",
+    "save_lives": "உயிர்களை காப்பாற்றவும்",
+    "made_with": "அன்புடன் உருவாக்கப்பட்டது",
+    "home": "முகப்பு பக்கம்",
+    "helpine": "இந்திய ஹெல்ப்லைன் சேவைகள்",
+    "search": "இங்கே தேடவும்...",
+    "new_complaint": "புதிய புகார்",
+    "child_det": "குழந்தையின் விவரங்கள்",
+    "name": "பெயர்",
+    "landmark": "இடத்தை உள்ளிடவும்",
+    "child_state": "குழந்தையின் நிலையை விவரிக்கவும்",
+    "low": "குறைந்த",
+    "high": "உயர்ந்த",
+    "moderate": "மிதமான",
+    "critical": "விமர்சனமானது",
+    "severity": "தீவிரத்தை நிவர்த்தி செய்யும் மதிப்பு",
+    "cat_class": "புகார் வகைப்பாடு",
+    "cat_class_desc":
+        "AI-இயங்கும் வகை வகைப்பாடு உங்கள் புகாரை திறம்பட வகைப்படுத்த எங்களுக்கு உதவுகிறது, இது விரைவான மற்றும் திறமையான வழக்கு கண்காணிப்புக்கு வழிவகுக்கும்",
+    "pred_cat": "கணிக்கப்பட்ட வகை",
+    "pred_cat_button": "வகையை கணிக்க",
+    "upload": "பதிவேற்றம்",
+    "photo": "குழந்தையின் புகைப்படத்தை இங்கே சேர்க்கவும்",
+    "your_det": "உங்கள் விவரங்கள்",
+    "phone_no": "தொலைபேசி எண்",
+    "enter_otp": "தயவுசெய்து OTP உள்ளிடவும்",
+    "verify_otp": "OTP சரிபார்க்கவும்",
+    "complaint_class": "புகார் வகைப்பாடு",
+    "slavery": "அடிமைத்தனம்",
+    "sexual_exploit": "பாலியல் சுரண்டல்",
+    "debt_bond": "கடன் அடிமைத்தனம்",
+    "trafficking": "கடத்தல்",
+    "forced_labor": "கட்டாய உழைப்பு"
+  };
+  static const Map<String, dynamic> en = {
+    "dashboard": "Dashboard",
+    "app_name": "Child Labour Tracking System",
+    "announcement": "Announcement",
+    "help": "Help",
+    "about": "About",
+    "logout": "Logout",
+    "stats1": "10 people are using CLTS to solve their grievances",
+    "post": "Post Complaint",
+    "post_line": "Post your complaints about child labour here",
+    "track": "Track Status",
+    "track_line": "Check the status of your registered complaints",
+    "save_lives": "SAVE LIVES",
+    "made_with": "MADE WITH LOVE",
+    "home": "Home",
+    "helpine": "Indian Helpline Services",
+    "search": "Search here...",
+    "new_complaint": "New Complaint",
+    "child_det": "Child's Details",
+    "name": "Name",
+    "landmark": "Enter landmark",
+    "child_state": "Describe the state of the child",
+    "low": "LOW",
+    "high": "HIGH",
+    "moderate": "MODERATE",
+    "critical": "CRITICAL",
+    "severity": "Severity Redressal Value",
+    "cat_class": "Category Classification",
+    "cat_class_desc":
+        "AI-powered category classification enables us to effectively categorize your complaint leading to faster and efficient case tracking",
+    "pred_cat": "Predicted category",
+    "pred_cat_button": "Predict category",
+    "upload": "Upload",
+    "photo": "Add child's photo here",
+    "your_det": "Your Details",
+    "phone_no": "Phone Number",
+    "enter_otp": "Please enter the OTP",
+    "verify_otp": "Verify OTP",
+    "complaint_class": "Complaint classification",
+    "slavery": "Slavery",
+    "sexual_exploit": "Sexual exploitation",
+    "debt_bond": "Debt bondage",
+    "trafficking": "Trafficking",
+    "forced_labor": "Forced labour"
+  };
+  static const Map<String, dynamic> hi = {
+    "dashboard": "डैशबोर्ड",
+    "app_name": "बाल श्रम ट्रैकिंग प्रणाली",
+    "announcement": "घोषणा",
+    "help": "सहायता",
+    "about": "इत्तिला",
+    "logout": "लॉग आउट",
+    "stats1": "10 लोग अपनी शिकायतों के समाधान के लिए CLTS का उपयोग कर रहे हैं",
+    "post": "शिकायत पोस्ट करें",
+    "post_line": "बाल श्रम के बारे में अपनी शिकायतें यहां पोस्ट करें",
+    "track": "स्थिति को ट्रैक करें",
+    "track_line": "अपनी पंजीकृत शिकायतों की स्थिति यहां देखें",
+    "save_lives": "जीवन बचाए",
+    "made_with": "प्यार से बनाया",
+    "home": "मुख पृष्ठ",
+    "helpine": "भारतीय हेल्पलाइन सेवाएं",
+    "search": "यहां तलाश करो...",
+    "new_complaint": "नई शिकायत",
+    "child_det": "बच्चे का विवरण",
+    "name": "नाम",
+    "landmark": "स्थान दर्ज करें",
+    "child_state": " बच्चे की स्थिति का वर्णन करें",
+    "low": " कम",
+    "high": "उच्च",
+    "moderate": "संतुलित",
+    "critical": "नाजुक",
+    "severity": "गंभीरता निवारण मूल्य",
+    "cat_class": "श्रेणी वर्गीकरण",
+    "cat_class_desc":
+        "एआई (AI) पावर्ड श्रेणी वर्गीकरण हमें आपकी शिकायत को प्रभावी ढंग से वर्गीकृत करने में सक्षम बनाता है जिससे तेजी से और कुशल केस ट्रैकिंग हो जाती है",
+    "pred_cat": "अनुमानित श्रेणी",
+    "pred_cat_button": "भविष्यवाणी श्रेणी",
+    "upload": "अपलोड करें",
+    "photo": "यहां बच्चे की फोटो लगाएं",
+    "your_det": "आपका विवरण",
+    "phone_no": "फ़ोन नंबर",
+    "enter_otp": "कृपया ओटीपी दर्ज करें",
+    "verify_otp": "ओटीपी सत्यापित करें",
+    "complaint_class": "शिकायत वर्गीकरण",
+    "slavery": "गुलामी",
+    "sexual_exploit": "यौन शोषण",
+    "debt_bond": "ऋण बंधन",
+    "trafficking": "तस्करी",
+    "forced_labor": "जबरन मज़दूरी कराना"
+  };
+  static const Map<String, Map<String, dynamic>> mapLocales = {
+    "ta": ta,
+    "en": en,
+    "hi": hi
+  };
 }
